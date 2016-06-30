673e79e9ee69431511be9fee632531c266b1d944
DarthDARKFather <artistsketchs@aol.com>
2016-06-30 02:37:33 +0000

    another indention

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
 		sequence :email do |n|
 		  "dummyEmail#{n}@gmail.com"
 		end
-	password "secretPassword"
-	password_confirmation "secretPassword"
+		password "secretPassword"
+		password_confirmation "secretPassword"
 
 	end
 end
