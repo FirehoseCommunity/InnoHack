34d28867adf811735b3cfa02991345496b17495c
Kamakshi <kpat108@gmail.com>
2016-07-20 01:28:49 +0000

    fixed indentations

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,66 +1,66 @@
 class SharesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 	
-	def index
-    @shares = Share.all
-	end
+  def index
+   @shares = Share.all
+  end
 
-	def new
-    @share = Share.new
-	end
+  def new
+   @share = Share.new
+  end
 
-	def create
-	  @share = current_user.shares.create(share_params)
-	  if @share.valid?
-	   redirect_to shares_path
-	  else
-	   render :new, status: :unprocessable_entity
-	  end
-	end
+  def create
+   @share = current_user.shares.create(share_params)
+   if @share.valid?
+    redirect_to shares_path
+   else
+    render :new, status: :unprocessable_entity
+   end
+  end
 
-	def show
-	  @share = Share.find_by_id(params[:id])
-	  return render_not_found if @share.blank?
-	end
+  def show
+   @share = Share.find_by_id(params[:id])
+   return render_not_found if @share.blank?
+  end
 
-	def edit
-	  @share = Share.find_by_id(params[:id])
-	  return render_not_found if @share.blank?
-	  return render_not_found(:forbidden) if @share.user != current_user
-	end
+  def edit
+   @share = Share.find_by_id(params[:id])
+   return render_not_found if @share.blank?
+   return render_not_found(:forbidden) if @share.user != current_user
+  end
 
-	def update
-	  @share = Share.find_by_id(params[:id])
-	  return render_not_found if @share.blank?
-	  return render_not_found(:forbidden) if @share.user != current_user
+  def update
+   @share = Share.find_by_id(params[:id])
+   return render_not_found if @share.blank?
+   return render_not_found(:forbidden) if @share.user != current_user
 	  
-	  @share.update_attributes(share_params)
-	  if @share.valid?
-	   redirect_to shares_path
-	  else
-	   return render :edit, status: :unprocessable_entity
-    end
-	end
+   @share.update_attributes(share_params)
+   if @share.valid?
+    redirect_to shares_path
+   else
+    return render :edit, status: :unprocessable_entity
+   end
+  end
 
-	def destroy
-	  @share = Share.find_by_id(params[:id])
-	  return render_not_found if @share.blank?
-	  return render_not_found(:forbidden) if @share.user != current_user
-	  @share.destroy
-	  redirect_to shares_path 
+  def destroy
+   @share = Share.find_by_id(params[:id])
+   return render_not_found if @share.blank?
+   return render_not_found(:forbidden) if @share.user != current_user
+   @share.destroy
+   redirect_to shares_path 
   end
 
   def upvote
-	  @share = Share.find(params[:id])
-	  @share.votes.create
-	  redirect_to shares_path
-	end
+   @share = Share.find(params[:id])
+   @share.votes.create
+   redirect_to shares_path
+  end
 
-	private
+  private
 
-	def share_params
-	  params.require(:share).permit(:body, :title)
-	end
+  def share_params
+   params.require(:share).permit(:body, :title)
+  end
 
 
 end
--- spec/controllers/shares_controller_spec.rb
+++ spec/controllers/shares_controller_spec.rb
@@ -1,162 +1,161 @@
 require 'rails_helper'
 
 RSpec.describe SharesController, type: :controller do
-	describe "shares#index action" do
-		it "should require users to be logged in" do
-			get :new
-			expect(response).to redirect_to new_user_session_path
-		end
+  describe "shares#index action" do
+   it "should require users to be logged in" do
+    get :new
+    expect(response).to redirect_to new_user_session_path
+   end
 
-		it "should successfully show the page" do
-			get :index
-			expect(response).to have_http_status(:success)
-		end
-	end
+   it "should successfully show the page" do
+    get :index
+    expect(response).to have_http_status(:success)
+   end
+  end
 
-	describe "shares#new action" do
-		it "should successfully show the new form" do
-			user = FactoryGirl.create(:user)
-			sign_in user
+  describe "shares#new action" do
+   it "should successfully show the new form" do
+    user = FactoryGirl.create(:user)
+    sign_in user
 
-			get :new
-			expect(response).to have_http_status(:success)
-		end
-	end
+    get :new
+    expect(response).to have_http_status(:success)
+   end
+  end
 
-	describe "shares#create action" do
-		it "should require users to be logged in" do
-			post :create, share: {body: 'Hack Attack!'}
-			expect(response).to redirect_to new_user_session_path
-		end
+  describe "shares#create action" do
+   it "should require users to be logged in" do
+    post :create, share: {body: 'Hack Attack!'}
+    expect(response).to redirect_to new_user_session_path
+   end
 
-		it "should successfully create a new share in our database" do
-			user = FactoryGirl.create(:user)
-			sign_in user
+   it "should successfully create a new share in our database" do
+    user = FactoryGirl.create(:user)
+    sign_in user
 
-			post :create, share: {body: 'Hack Attack!'}
-			expect(response).to redirect_to shares_path
+    post :create, share: {body: 'Hack Attack!'}
+    expect(response).to redirect_to shares_path
 
-			share = Share.last
-			expect(share.body). to eq("Hack Attack!")
-		end
+    share = Share.last
+    expect(share.body). to eq("Hack Attack!")
+   end
 
-		it "should properly deal with validation errors" do
-			user = FactoryGirl.create(:user)
-			sign_in user
+   it "should properly deal with validation errors" do
+    user = FactoryGirl.create(:user)
+    sign_in user
 
-			post :create, share: {body: ''}
-			expect(response).to have_http_status(:unprocessable_entity)
-		end
-	end
+    post :create, share: {body: ''}
+    expect(response).to have_http_status(:unprocessable_entity)
+   end
+  end
 
-	describe "shares#show action" do
-		it "should successfully show a share" do
-			share = FactoryGirl.create(:share)
-			get :show, id: share.id
-		end
+  describe "shares#show action" do
+   it "should successfully show a share" do
+    share = FactoryGirl.create(:share)
+    get :show, id: share.id
+   end
 
-		it "should return a 404 error if the share is not found" do
-			get :show, id: 'NOTHING HERE'
-			expect(response).to have_http_status(:not_found)
-		end 
-	end
+   it "should return a 404 error if the share is not found" do
+    get :show, id: 'NOTHING HERE'
+    expect(response).to have_http_status(:not_found)
+   end 
+  end
 
-	describe "shares#edit" do
-		it "shouldn't let a user who did not create the share edit a share" do
-    	share = FactoryGirl.create(:share)
-   		user = FactoryGirl.create(:user)
-    	sign_in user
+  describe "shares#edit" do
+   it "shouldn't let a user who did not create the share edit a share" do
+    share = FactoryGirl.create(:share)
+    user = FactoryGirl.create(:user)
+    sign_in user
 
-    	get :edit, id: share.id
-    	expect(response).to have_http_status(:forbidden)     
-		end
+    get :edit, id: share.id
+    expect(response).to have_http_status(:forbidden)     
+   end
 
-		it "shouldn't let unauthenticated users edit a share" do
-    	share = FactoryGirl.create(:share)
-    	get :edit, id: share.id
-    	expect(response).to redirect_to new_user_session_path
-		end
+   it "shouldn't let unauthenticated users edit a share" do
+    share = FactoryGirl.create(:share)
+    get :edit, id: share.id
+    expect(response).to redirect_to new_user_session_path
+   end
 
-		it "should successfully show the edit form if the share is found" do
-    	share = FactoryGirl.create(:share)
-     sign_in share.user
-     get :edit, id: share.id
-     expect(response).to have_http_status(:success)
-	  end
+   it "should successfully show the edit form if the share is found" do
+    share = FactoryGirl.create(:share)
+    sign_in share.user
+    get :edit, id: share.id
+    expect(response).to have_http_status(:success)
+   end
 
-		it "should return a 404 error message if the share is not found" do
-     user = FactoryGirl.create(:user)
-     sign_in user
-     get :edit, id: 'NOT FOUND'
-     expect(response).to have_http_status(:not_found)
-	  end
-	end
+   it "should return a 404 error message if the share is not found" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    get :edit, id: 'NOT FOUND'
+    expect(response).to have_http_status(:not_found)
+   end
+  end
 
-	describe "shares#update" do
-		it "shouldn't let users who did create the share update it" do
-     share = FactoryGirl.create(:share)
-     user = FactoryGirl.create(:user)
-     sign_in user
-     patch :update, id: share.id, share: {body: 'Hacked'}
-     expect(response).to have_http_status(:forbidden)
-		end	
+  describe "shares#update" do
+   it "shouldn't let users who did create the share update it" do
+    share = FactoryGirl.create(:share)
+    user = FactoryGirl.create(:user)
+    sign_in user
+    patch :update, id: share.id, share: {body: 'Hacked'}
+    expect(response).to have_http_status(:forbidden)
+   end	
 
-		it "shouldn't let unauthenticated users create a share" do
-     share = FactoryGirl.create(:share)
-     patch :update, id: share.id, share: {body: 'Hack Attack'}
-     expect(response).to redirect_to new_user_session_path
-		end
+   it "shouldn't let unauthenticated users create a share" do
+    share = FactoryGirl.create(:share)
+    patch :update, id: share.id, share: {body: 'Hack Attack'}
+    expect(response).to redirect_to new_user_session_path
+   end
 
-		it "should allow users to successfully update shares" do
-     share = FactoryGirl.create(:share, body: "Initial Value")
-     sign_in share.user
-     patch :update, id: share.id, share: {body: "Updated"}
-		 expect(response).to redirect_to shares_path
-		end
+   it "should allow users to successfully update shares" do
+    share = FactoryGirl.create(:share, body: "Initial Value")
+    sign_in share.user
+    patch :update, id: share.id, share: {body: "Updated"expect(response).to redirect_to shares_path
+   end
 
-		it "should have http 404 error if the share cannot be found" do
-     user = FactoryGirl.create(:user)
-     sign_in user
-     patch :update, id: "DOES NOT EXIST", share: {body: "Updated"}
-     expect(response).to have_http_status(:not_found)
-		end
+   it "should have http 404 error if the share cannot be found" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    patch :update, id: "DOES NOT EXIST", share: {body: "Updated"}
+    expect(response).to have_http_status(:not_found)
+   end
 
-		it "should render the edit form with an http status of unprocessable_entity" do
-		 share = FactoryGirl.create(:share, body: "Initial Value")
-		 sign_in share.user
-		 patch :update, id: share.id, share: {body: '' }
-		 expect(response).to have_http_status(:unprocessable_entity)
-		end
-	end
+   it "should render the edit form with an http status of unprocessable_entity" do 
+    share = FactoryGirl.create(:share, body: "Initial Value"
+    sign_in share.use
+    patch :update, id: share.id, share: {body: '' }
+    expect(response).to have_http_status(:unprocessable_entity)
+   end
+  end
 
-	describe "shares#destroy" do
-	  it "shouldn't allow user who did not create the share to destroy it" do
- 		 share = FactoryGirl.create(:share)
- 		 user = FactoryGirl.create(:user)
- 		 sign_in user
- 		 delete :destroy, id: share.id
- 		 expect(response).to have_http_status(:forbidden)
-	  end
+  describe "shares#destroy" do
+   it "shouldn't allow user who did not create the share to destroy it" do
+    share = FactoryGirl.create(:share)
+    user = FactoryGirl.create(:user)
+    sign_in user
+    delete :destroy, id: share.id
+    expect(response).to have_http_status(:forbidden)
+   end
 
-	  it "shouldn't let unauthenticated users destroy a share" do
-		 share = FactoryGirl.create(:share)
-		 delete :destroy, id: share.id
-		 expect(response).to redirect_to new_user_session_path
-		end
+   it "shouldn't let unauthenticated users destroy a share" do
+    share = FactoryGirl.create(:share)
+    delete :destroy, id: share.id
+    expect(response).to redirect_to new_user_session_path
+   end
 
-		it "should allow a user to destroy shares" do
-			share = FactoryGirl.create(:share)
-			sign_in share.user
-			delete :destroy, id: share.id
-			expect(response).to redirect_to shares_path
-		end
+   it "should allow a user to destroy shares" do
+    share = FactoryGirl.create(:share)
+    sign_in share.user
+    delete :destroy, id: share.id
+    expect(response).to redirect_to shares_path
+   end
 
-		it "should return a 404 message if we cannot find a share with the id specified" do
-			user = FactoryGirl.create(:user)
-			sign_in user
-			delete :destroy, id: 'SHARE IS NOT HERE'
-			expect(response).to have_http_status(:not_found)
-		end
-	end
+   it "should return a 404 message if we cannot find a share with the id specified" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    delete :destroy, id: 'SHARE IS NOT HERE'
+    expect(response).to have_http_status(:not_found)
+   end
+  end
 
 end
