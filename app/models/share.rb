f146c5d056e4f9242623324a5ee5355738517fb4
Kamakshi <kpat108@gmail.com>
2016-07-31 20:15:05 +0000

    added chronological order of shares with newest first

--- app/models/share.rb
+++ app/models/share.rb
@@ -6,5 +6,7 @@ class Share < ActiveRecord::Base
   belongs_to :user, foreign_key: 'user_id', class_name: "User"
   has_many :votes
   has_many :comments
+
+  scope :newest, -> {order("updated_at DESC")}
   
 end
