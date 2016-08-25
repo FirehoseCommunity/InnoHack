7ef62386aff5253b63017b1aba0657d96fc70a4f
Kamakshi <kpat108@gmail.com>
2016-08-25 00:37:31 +0000

    fixed toastr links

---
+++ app/assets/javascripts/skins.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  // $('.skin').click(function(){
+  //   $('body').css('background-image', 'url("' + this.value + '")');
+    
+  // });
+
+
+  $('#btn1').click(function(){
+    $('body').css('background-image', 'url(Skin1.png)');
+  });
+});
