e3f737cadce0d6ca948ee6fb90d93802187cb949
Maile Thiesen <mailethiesen@gmail.com>
2016-09-01 01:53:54 +0000

    fixed notes from ken, added no turbo links to redirects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820225504) do
+ActiveRecord::Schema.define(version: 20160901013042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20160820225504) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "lat"
-    t.string   "lon"
+    t.float    "lat"
+    t.float    "lon"
   end
 
   create_table "simple_hashtag_hashtaggings", force: :cascade do |t|
