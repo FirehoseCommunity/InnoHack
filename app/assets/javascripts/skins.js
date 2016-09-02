5ed01228c4658a860f5c6ada066b14ccd30517a5
Kamakshi <kpat108@gmail.com>
2016-09-02 22:28:35 +0000

    fixing conflicts

---
+++ app/assets/javascripts/skins.js
@@ -0,0 +1,40 @@
+$(function() {
+
+
+  var theme = $.cookie('backgroundImage');
+  
+  if (theme) {
+    document.getElementById("box").style.backgroundImage = "url("+theme+")";
+  }
+  
+  $('#btn1').click(function() {
+    theme = "/assets/skin_1.png";
+    $.cookie('backgroundImage', theme, {expires: 2});
+  });
+
+  $('#btn2').click(function() {
+    theme = "/assets/skin_2.png";
+    $.cookie('backgroundImage', theme, {expires: 2});
+  });
+
+   $('#btn3').click(function() {
+    theme = "/assets/skin_3.png";
+    $.cookie('backgroundImage', theme, {expires: 2});
+  });
+
+ // var theme = $.cookie('backgroundImage');
+  
+ //  if (theme) {
+ //    document.getElementById("box").style.backgroundImage = "url("+theme+")";
+ //  }
+  
+ //  $('.skins').click(function(){
+ //    theme = $(this).attr('src');
+ //    $.cookie('backgroundImage', theme, {expires: 2});
+ //  });
+
+ 
+
+
+
+});
